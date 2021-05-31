337a47fb48131a6bbd61f231d804d50dc00a418e
Kellie Mogg <kelliedmogg@gmail.com>
2021-05-31 20:03:58 +0000

    installing buildpack

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 36.1516, lng: 95.9885 },
+    center: { lat: 36.1516, lng: -95.9885 },
     zoom: 8,
   });
 }
